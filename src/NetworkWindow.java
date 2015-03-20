04735644ba850a845466356fbcdfcc68a7edfb49
Bryan Muscedere <bmuscede@uwo.ca>
2015-03-20 16:22:38 -0400

    Started work on selecting individual nodes to draw.

--- src/NetworkWindow.java
+++ src/NetworkWindow.java
@@ -10,7 +10,6 @@ import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,15 +19,32 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import org.apache.commons.collections15.Factory;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JToolBar;
 
-public class NetworkWindow extends JFrame {
+public class NetworkWindow extends JFrame implements ActionListener {
 	private static final long serialVersionUID = -1880325660041879306L;
 	
+	//Graph items.
 	private Graph<Node, Link> graph;
     private int nodeCount, edgeCount;
+    private EditingModalGraphMouse<Node, Link> gm;
+    
+    //Swing items.
 	private JPanel contentPane;
-
+	private String[] iconFiles = {"dsl_end.gif", "voip_end.gif", "edge.gif", "core.gif", "gateway.gif", "transform.gif" };
+	private String[] buttonLabels = {"Create DSL Customer Node", 
+			 						 "Create VoIP Customer Node", 
+			 					     "Create Edge Router", 
+			 					     "Create Core Router", 
+			 					     "Create Gateway Router", 
+			 					     "Transform" };
+	
+	//Enum for mode.
+	private enum GraphMode { DSL_END, VOIP_END, EDGE, CORE, GATEWAY };
+	private GraphMode state = null;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -63,20 +79,14 @@ public class NetworkWindow extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JPanel pnlGraph = new JPanel();
+		Layout<Node, Link> layout = new StaticLayout<Node, Link>(graph);
+		VisualizationViewer<Node, Link> pnlGraph = new VisualizationViewer<Node, Link>(layout);
 		pnlGraph.setBackground(Color.WHITE);
 		contentPane.add(pnlGraph, BorderLayout.CENTER);
 		
 		JToolBar tblOperations = new JToolBar();
-		
+
 		//Generate image operations.
-		String[] iconFiles = {"dsl_end.gif", "voip_end.gif", "edge.gif", "core.gif", "gateway.gif", "link.gif" };
-		String[] buttonLabels = {"Create DSL Customer Node", 
-								 "Create VoIP Customer Node", 
-								 "Create Edge Router", 
-								 "Create Core Router", 
-								 "Create Gateway Router", 
-								 "Draw Link" };
 		ImageIcon[] icons = new ImageIcon[iconFiles.length];
 		JButton[] buttons = new JButton[buttonLabels.length];
 		  
@@ -84,6 +94,7 @@ public class NetworkWindow extends JFrame {
 		for (int i = 0; i < buttonLabels.length; ++i) {
 			icons[i] = new ImageIcon(iconFiles[i]);
 		    buttons[i] = new JButton(icons[i]);
+		    buttons[i].addActionListener(this);
 		    buttons[i].setToolTipText(buttonLabels[i]);
 		      
 		    //Check to see if we need a separator.
@@ -94,12 +105,45 @@ public class NetworkWindow extends JFrame {
 		}
 	
 		contentPane.add(tblOperations, BorderLayout.NORTH);
+		
+		//Now we create a node/link labeler system.
+        pnlGraph.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<Node>());
+        pnlGraph.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller<Link>());
+        
+        //Allow the user to add items using their mouse.
+        gm = new EditingModalGraphMouse<Node, Link>(pnlGraph.getRenderContext(), null, null); 
+        pnlGraph.setGraphMouse(gm);
+        
+        //Start the graph off in transforming mode.
+        gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
 	}
 	
 	private void createGraph(){
 		//Create the graph object and the nodes and link.
         graph = new UndirectedSparseMultigraph<Node, Link>();
         nodeCount = 0; edgeCount = 0;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event){
+		//Get the source of the click.
+		JButton clicked = (JButton) event.getSource();
+		String tooltip = clicked.getToolTipText();
+		
+		//We now see which button in the toolbar was pressed.
+		if (tooltip.equals(buttonLabels[0])){
+			//We set the mode to edit.
+			gm.setMode(ModalGraphMouse.Mode.EDITING);
+			
+			//Now we indicate the type of node.
+			state = GraphMode.DSL_END;
+		} else if (tooltip.equals(buttonLabels[5])){
+			//We set the mode to transform.
+			gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
+			
+			//Now we indicate the type of node.
+			state = null;
+		}
 	}
 
 }
